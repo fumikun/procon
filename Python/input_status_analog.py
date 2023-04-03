7c1b508ac28cbf6c03c2bf5c11f006cbc4a7d3ff
fumikun <fumikun6130@gmail.com>
2023-04-03 20:52:20 +0900

    スティック操作プログラム分離

---
+++ Python/input_status_analog.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+def Analog_status_reset(screen):
+    pygame.draw.ellipse(screen, (0,0,0), (600, 225, 50, 50))
+    pygame.draw.ellipse(screen, (0,0,0), (850, 225, 50, 50))
+
+def Analog_status_show(pygame, joystick, sensitivity, screen, bgcolor):
+    if abs((joystick.get_axis(0))) >= sensitivity or abs((joystick.get_axis(1))) >= sensitivity:
+        pygame.draw.ellipse(screen, bgcolor, (600, 225, 50, 50))
+        pygame.draw.ellipse(screen, (0,0,0), (625 + joystick.get_axis(0) * 125 -25, 250 + joystick.get_axis(1) * 125 -25, 50, 50))
+        pygame.display.update()
+    elif abs((joystick.get_axis(2))) >= sensitivity or abs((joystick.get_axis(3))) >= sensitivity:
+        pygame.draw.ellipse(screen, bgcolor, (850, 225, 50, 50))
+        pygame.draw.ellipse(screen, (0,0,0), (875 + joystick.get_axis(2) * 125 -25, 250 + joystick.get_axis(3) * 125 -25, 50, 50))
+        pygame.display.update()
--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from pygame.locals import *
 import time
 import sys
 from Python import input_status_button as button
+from Python import input_status_analog as analog
 
 def main():
     # 設定
     pygame.init()
-    sensitivity = 0.5 # ジョイスティック感度設定
-    font1 =pygame.font.SysFont("arial", 20) # フォント設定
-    accent_color = (30,227,207,1)
-    font_color =(0,0,0)
-    text_top = 350
-    text_start1 = 20
-    text_start2 = 170
-    text_start3 = 320
+    sensitivity = 0.5 # スティック感度設定
+    accent_color = (30,227,207,1) # ON時の色
+    text_top = 350 # ボタンインジケーターの1番上
+    text_start1 = 20 # タンインジケーター1列目左端
+    text_start2 = 170 # ボタンインジケーター2列目左端
+    text_start3 = 320 # ボタンインジケーター3列目左端
+    bgcolor = (242,244,246,1)
 
     # ジョイスティックの初期化
     pygame.joystick.init()
@@ -29,16 +29,19 @@ def main():
 
     # 画面の生成
     screen = pygame.display.set_mode((1000, 500))
-    pygame.display.set_caption('Nintendo Switch Pro Controller')
+    pygame.display.set_caption(joystick.get_name() + " " +str(joystick.get_numbuttons()) +" Buttons, " + str(joystick.get_numaxes()) +" Sticks")
     bg = pygame.image.load("img/procon.png").convert_alpha()
     rect_bg = bg.get_rect()
 
     while True:
-        screen.fill((242,244,246,1))
+        screen.fill(bgcolor)
         screen.blit(bg, rect_bg)
+        pygame.draw.ellipse(screen, bgcolor, (500, 125, 250, 250))
+        pygame.draw.ellipse(screen, bgcolor, (750, 125, 250, 250))
         pygame.draw.ellipse(screen, (0, 0, 0), (500, 125, 250, 250), 5)
         pygame.draw.ellipse(screen, (0, 0, 0), (750, 125, 250, 250), 5)
-        button.Button_status(screen, text_start1, text_start2, text_start3, text_top)
+        button.Button_status_reset(screen, text_start1, text_start2, text_start3, text_top)
+        analog.Analog_status_reset(screen)
         # イベントの取得
         for e in pygame.event.get():
             if e.type == QUIT:
@@ -48,14 +51,7 @@ def main():
             # ジョイスティックのボタンの入力
 
             elif e.type == pygame.JOYAXISMOTION:
-                        if abs((joystick.get_axis(0))) >= sensitivity or abs((joystick.get_axis(1))) >= sensitivity:
-                            # print("("+str(joystick.get_axis(0))+","+ str(joystick.get_axis(1))+")")
-                            pygame.draw.ellipse(screen, (0,100,0), (625 + joystick.get_axis(0) * 125, 250 + joystick.get_axis(1) * 125, 50, 50))
-                            pygame.display.update()
-                        elif abs((joystick.get_axis(2))) >= sensitivity or abs((joystick.get_axis(3))) >= sensitivity:
-                            # print("("+str(joystick.get_axis(2))+","+ str(joystick.get_axis(3))+")")
-                            pygame.draw.ellipse(screen, (0,100,0), (875, 250, 50, 50))
-                            pygame.display.update()
+                        analog.Analog_status_show(pygame, joystick, sensitivity, screen, bgcolor)
             elif e.type == pygame.locals.JOYBUTTONDOWN:
                 button.button_status_show_on(joystick, screen, accent_color, text_start1, text_start2, text_start3, text_top)
                 pygame.display.update()
