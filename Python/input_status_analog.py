a2d8a0e02f572e5a178b29a221df46b4b3a5f012
fumikun <111163084+fumikun@users.noreply.github.com>
2023-04-04 07:26:00 +0900

    中心回帰処理

--- Python/input_status_analog.py
+++ Python/input_status_analog.py
@@ -14,3 +14,9 @@ def Analog_status_show(pygame, joystick, sensitivity, screen, bgcolor):
         pygame.draw.ellipse(screen, bgcolor, (850, 225, 50, 50))
         pygame.draw.ellipse(screen, (0,0,0), (875 + joystick.get_axis(2) * 125 -25, 250 + joystick.get_axis(3) * 125 -25, 50, 50))
         pygame.display.update()
+    if abs((joystick.get_axis(0))) <= sensitivity or abs((joystick.get_axis(1))) <= sensitivity:
+        pygame.draw.ellipse(screen, (0,0,0), (500, 125, 250, 250), 5)
+        pygame.display.update()
+    elif abs((joystick.get_axis(2))) <= sensitivity or abs((joystick.get_axis(3))) <= sensitivity:
+        pygame.draw.ellipse(screen, (0,0,0), (750, 125, 250, 250), 5)
+        pygame.display.update()
